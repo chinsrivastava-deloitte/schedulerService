5558872cf8f0211e364d1edcd73865588535938f
nishant20-deloitte <niskashyap@deloitte.com>
2022-05-31 18:29:26 +0530

    deletion from repo and bloodPressure service

--- src/main/java/com/example/testpipeline/repositories/BloodOxygenRepository.java
+++ src/main/java/com/example/testpipeline/repositories/BloodOxygenRepository.java
@@ -2,8 +2,18 @@ package com.example.testpipeline.repositories;
 
 import com.example.testpipeline.models.BloodOxygenData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface BloodOxygenRepository extends JpaRepository<BloodOxygenData,Integer> {
+    @Modifying
+    @Query(
+            value = "DELETE FROM blood_oxygen_data where `timestamp`<(NOW()-INTERVAL 9 MINUTE)",
+            nativeQuery = true
+    )
+    public void deleteBefore();
 }
